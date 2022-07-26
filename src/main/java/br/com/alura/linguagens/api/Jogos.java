7d23f29f0f06288b8d625a418b42d1e7fab38c5d
Fábio Mori <fsmmori@gmail.com>
2022-07-26 10:57:16 -0300

    make it better

--- src/main/java/br/com/alura/linguagens/api/Jogos.java
+++ src/main/java/br/com/alura/linguagens/api/Jogos.java
@@ -3,7 +3,7 @@ package br.com.alura.linguagens.api;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document(collection = "jogosKIDS")
+@Document(collection = "principaisLinguagens")
 public class Jogos {
 
     @Id
