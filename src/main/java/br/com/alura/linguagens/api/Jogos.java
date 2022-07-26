bf2f8d864004510f994ecf507ac3ac529726c58b
Fábio Mori <fsmmori@gmail.com>
2022-07-26 15:10:36 -0300

    make it better

--- src/main/java/br/com/alura/linguagens/api/Jogos.java
+++ src/main/java/br/com/alura/linguagens/api/Jogos.java
@@ -3,42 +3,47 @@ package br.com.alura.linguagens.api;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document(collection = "matrizKIDS")
+@Document(collection = "jogosKIDS")
 public class Jogos {
 
     @Id
     private String id;
     private String title;
-    private String link;
-    private int ranking;
+   // private String link;
     private String image;
+    private int ranking;
+    
 
     public Jogos() {
 
     }
 
-    public Jogos(String title, String link, int ranking, String image) {
+    //public Jogos(String title, String link, int ranking, String image) {
+    public Jogos(String title, String image, int ranking) {
         this.title = title;
-        this.link = link;
-        this.ranking = ranking;
+        //this.link = link;
         this.image = image;
+        this.ranking = ranking;
+        
     }
 
     public void setTitle(String title) {
         this.title = title;
     }
 
-    public void setLink(String link) {
-        this.link = link;
-    }
+//    public void setLink(String link) {
+//        this.link = link;
+//    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }   
 
     public void setRanking(int ranking) {
         this.ranking = ranking;
     }
 
-    public void setImage(String image) {
-        this.image = image;
-    }
+
 
     public String getId() {
         return id;
@@ -47,15 +52,16 @@ public class Jogos {
     public String getTitle() {
         return title;
     }
-    public String getLink() {
-        return link;
-    }
-    public int getRanking() {
-        return ranking;
-    }
     public String getImage() {
         return image;
     }
+//    public String getLink() {
+ //       return link;
+//    }
+    public int getRanking() {
+        return ranking;
+    }
+
 
 
 }
